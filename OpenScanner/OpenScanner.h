cb69be4ec12d4ff24e8871b985a374a831edc9c5
Shubham Kamdi <shubhamvkamdi@gmail.com>
2022-12-25 13:36:27 -0800

    Initial Commit

---
+++ OpenScanner/OpenScanner.h
@@ -0,0 +1,18 @@
+//
+//  OpenScanner.h
+//  OpenScanner
+//
+//  Created by Shubham Kamdi on 12/25/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OpenScanner.
+FOUNDATION_EXPORT double OpenScannerVersionNumber;
+
+//! Project version string for OpenScanner.
+FOUNDATION_EXPORT const unsigned char OpenScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenScanner/PublicHeader.h>
+
+
